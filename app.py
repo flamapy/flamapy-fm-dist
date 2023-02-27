225dcb5705d7acded2c3d41f5e105ff95d8faf6d
joszamama <joszamama@gmail.com>
2023-02-27 23:59:19 +0100

    feat: add swagger docs

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
+
+# Importing routes
 from routes.validator_routes import validator_bp
 
+# Creating the app and configuring it
 app = Flask(__name__)
-
 app.config['BASE_ROUTE'] = '/api/v1'
 
+# Swagger UI
+SWAGGER_URL = '/api/v1/docs'
+API_URL = '/static/swagger.yml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "FLAMAPY API - V1"
+    }
+)
+
+# Adding blueprints
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 app.register_blueprint(validator_bp)
+
 
 @app.route(app.config['BASE_ROUTE'], methods=['GET'])
 def hello_world():
