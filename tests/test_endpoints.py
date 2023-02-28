8c3c0a071f05958195d167ef6c43edc713cb1cda
joszamama <joszamama@gmail.com>
2023-02-28 12:33:02 +0100

    feat: add unit tests for all new methods

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,11 +1,9 @@
 import sys
-sys.path.append("..")
-
-from app import app
 import pytest
 import json
 
-
+sys.path.append("..")
+from app import app
 
 def test_hello_world():
     client = app.test_client()
@@ -14,3 +12,30 @@ def test_hello_world():
     assert response.status_code == 200
 
 
+def test_get_plugins():
+    # Arrange
+    client = app.test_client()
+
+    # Act
+    response = client.get(app.config['API_BASE_URL'] + '/info/plugins')
+    result = json.loads(response.data)
+
+    # Assert
+    assert response.status_code == 200
+    assert len(result) > 0
+
+
+def test_get_operations():
+    # Arrange
+    client = app.test_client()
+
+    # Act
+    response = client.get(app.config['API_BASE_URL'] + '/info/pysat_metamodel/operations')
+
+    # Assert
+    assert response.status_code == 308
+    response = client.get(response.headers['Location'])
+
+    assert response.status_code == 200
+    result = json.loads(response.data)
+    assert len(result) > 0
--- tests/test_units.py
+++ tests/test_units.py
@@ -1,11 +1,38 @@
 import sys
 import warnings
-from app import app
 import pytest
-from operations.validate import model_validator, product_validator
 
 sys.path.append("..")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from app import app
+from operations.validate import model_validator, product_validator
+from operations.count import count_valid_products, count_leafs
+from operations.find import find_valid_products, find_core_features
+from operations.info import get_plugins, get_operations
+
+
+
+def test_get_plugins():
+    # Arrange
+
+    # Act
+    result = get_plugins()
+
+    # Assert
+    assert len(result) > 0
+
+
+def test_get_operations():
+    # Arrange
+    plugin = "pysat_metamodel"
+
+    # Act
+    result = get_operations(plugin)
+
+    # Assert
+    assert len(result) > 0
+
 
 def test_model_validator_valid_model():
     # Arrange
@@ -17,6 +44,7 @@ def test_model_validator_valid_model():
     # Assert
     assert result == True
 
+
 def test_model_validator_invalid_model():
     # Arrange
     invalid_model = "../operations/products/valid_product.csv"
@@ -26,3 +54,70 @@ def test_model_validator_invalid_model():
 
     # Assert
     assert result == False
+
+# def test_product_validator_valid_product():
+#     # Arrange
+#     valid_model = "../operations/products/valid_model.uvl"
+#     valid_product = "../operations/products/valid_product.csv"
+
+#     # Act
+#     result = product_validator(valid_model, valid_product)
+
+#     # Assert
+#     assert result == True
+
+# def test_product_validator_invalid_product():
+#     # Arrange
+#     valid_model = "../operations/products/valid_model.uvl"
+#     invalid_product = "../operations/products/invalid_product.csv"
+
+#     # Act
+#     result = product_validator(valid_model, invalid_product)
+
+#     # Assert
+#     assert result == False
+
+
+def test_count_valid_products():
+    # Arrange
+    valid_model = "../operations/models/valid_model.uvl"
+
+    # Act
+    result = count_valid_products(valid_model)
+
+    # Assert
+    assert result == 816
+
+
+def test_count_leafs():
+    # Arrange
+    valid_model = "../operations/models/valid_model.uvl"
+
+    # Act
+    result = count_leafs(valid_model)
+
+    # Assert
+    assert result == 17
+
+
+def test_find_valid_products():
+    # Arrange
+    valid_model = "../operations/models/valid_model.uvl"
+
+    # Act
+    result = find_valid_products(valid_model)
+
+    # Assert
+    assert len(result) > 0
+
+
+def test_find_core_features():
+    # Arrange
+    valid_model = "../operations/models/valid_model.uvl"
+    core_features = ["eCommerce", "Server", "Web", "Catalog", "Search",
+                     "Shopping", "Security",  "Cart", "Payment", "PHP", "Storage", "v74"]
+    # Act
+    result = find_core_features(valid_model)
+
+    # Assert
+    assert result == core_features
