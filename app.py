e7c4b049590b712801d3da51c6bd64ae6b1c4483
joszamama <joszamama@gmail.com>
2023-02-28 12:06:45 +0100

    feat: add info, find and count routes

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 # Importing routes
 from routes.info_routes import info_bp
-from routes.validator_routes import validator_bp
+from routes.validate_routes import validate_bp
+from routes.count_routes import count_bp
+from routes.find_routes import find_bp
 
 # Creating the app and configuring it
 app = Flask(__name__)
@@ -23,7 +25,9 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 # Adding blueprints
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 app.register_blueprint(info_bp)
-app.register_blueprint(validator_bp)
+app.register_blueprint(validate_bp)
+app.register_blueprint(count_bp)
+app.register_blueprint(find_bp)
 
 
 @app.route(app.config['API_BASE_URL'], methods=['GET'])
