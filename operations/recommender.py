0f7ef3d50eb8ea358be3150fc9b9e7a4bc2ec6b1
joszamama <joszamama@gmail.com>
2023-04-28 20:38:37 +0200

    feat: add restrictiveness analysis

--- operations/recommender.py
+++ operations/recommender.py
@@ -5,7 +5,15 @@ from typing import NewType
 from flamapy.core.discover import DiscoverMetamodels
 
 
-def check_recommendation_objects(model, products, query):
+def check_recommendation_objects(model, products, query=None):
+    """
+    Check if the objects are valid for the recommendation operation.
+    :param model: The model file.
+    :param products: The products file.
+    :param query: The query file.
+    :return: 1 if the objects are valid, an error message otherwise.
+    """
+
     dm = DiscoverMetamodels()
     try:
         fm = dm.use_transformation_t2m(model, "fm")
@@ -30,26 +38,35 @@ def check_recommendation_objects(model, products, query):
         error = "The product file does not exist or is not a valid csv file."
         return error
 
-    try:
-        query_features = []
-        with open(query, newline='') as csvfile:
-            csv_reader = csv.reader(csvfile)
-            for row in csv_reader:
-                if row[1]:
-                    query_features.append(row[0])
+    if query is not None:
+        try:
+            query_features = []
+            with open(query, newline='') as csvfile:
+                csv_reader = csv.reader(csvfile)
+                for row in csv_reader:
+                    if row[1]:
+                        query_features.append(row[0])
 
-        for feature in query_features:
-            if feature not in features:
-                error = "The query file does not match the model."
-                return error
-    except:
-        error = "The query file does not exist or is not a valid csv file."
-        return error
+            for feature in query_features:
+                if feature not in features:
+                    error = "The query file does not match the model."
+                    return error
+        except:
+            error = "The query file does not exist or is not a valid csv file."
+            return error
 
     return 1
 
 
 def get_recommendations(model, products, query):
+    """
+    Get recommendations for a given query.
+    :param model: The model file.
+    :param products: The products file.
+    :param query: The query file.
+    :return: A dictionary with the recommendations.
+    """
+
     dm = DiscoverMetamodels()
 
     message = check_recommendation_objects(model, products, query)
@@ -87,8 +104,57 @@ def get_recommendations(model, products, query):
                 if configuration == features:
                     recommendations[product] = configuration
 
+        recommendations = dict(
+            sorted(recommendations.items(), key=lambda item: len(item[1]), reverse=True))
+
         os.remove(query_csvconf)
         return recommendations
     except:
         os.remove(query_csvconf)
         print("No product recommendations found.")
+
+
+def consistent_configurations(model, products):
+    dm = DiscoverMetamodels()
+
+    message = check_recommendation_objects(model, products)
+    if message != 1:
+        return message
+
+    try:
+        configurations = dm.use_operation_from_file('Products', model)
+        configurations = {i: configuration for i,
+                          configuration in enumerate(configurations)}
+        for value in configurations.values():
+            value.sort()
+
+        with open(products, newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            products = {rows[0].replace("product=", ""): [i.replace("=true", "") for i in rows[1:] if
+                                                          '=true' in i] for rows in csv_reader}
+            for value in products.values():
+                value.sort()
+
+        consistent_products = {}
+        for configuration in configurations.values():
+            for product, features in products.items():
+                if configuration == features:
+                    consistent_products[product] = configuration
+
+        consistent_products = dict(sorted(
+            consistent_products.items(), key=lambda item: len(item[1]), reverse=True))
+
+        return products, consistent_products
+    except:
+        print("No consistent configurations found.")
+
+
+def restrictiveness(model, products, feature_list):
+    products, consistent_products = consistent_configurations(model, products)
+
+    valid_products = {}
+    for product, features in consistent_products.items():
+        if all(feature in features for feature in feature_list):
+            valid_products[product] = features
+
+    return len(valid_products) / len(products) * 100
--- routes/recommender_routes.py
+++ routes/recommender_routes.py
@@ -1,6 +1,6 @@
 import os
 from flask import Blueprint, request, jsonify
-from operations.recommender import check_recommendation_objects, get_recommendations
+from operations.recommender import check_recommendation_objects, get_recommendations, restrictiveness
 
 recommender_bp = Blueprint('recommender_bp', __name__,
                            url_prefix='/api/v1/recommendations')
@@ -74,6 +74,40 @@ def recommend():
 
         # Return result
         return result
+
+    # If no file is provided
+    else:
+        return 'No file uploaded'
+
+
+@recommender_bp.route('/analysis/restrictiveness', methods=['POST'])
+def restrictiveness_route():
+
+    # Get files
+    model = request.files['model']
+    products = request.files['products']
+    features = request.form.getlist('features')
+    
+    # split by comma
+    features = [x.strip() for x in features[0].split(',')]
+
+    # Check if files are provided
+    if model.filename != '' and products.filename != '' and len(features) != 0:
+
+        # Save files
+        model.save(os.path.join(MODEL_FOLDER, model.filename))
+        products.save(os.path.join(PRODUCT_FOLDER, products.filename))
+
+        # Result
+        result = restrictiveness(os.path.join(MODEL_FOLDER, model.filename), os.path.join(
+            PRODUCT_FOLDER, products.filename), features)
+
+        # Remove files
+        os.remove(os.path.join(MODEL_FOLDER, model.filename))
+        os.remove(os.path.join(PRODUCT_FOLDER, products.filename))
+
+        # Return result
+        return str(result)
 
     # If no file is provided
     else:
